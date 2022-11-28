63c4398a76ed7e7a6d5f6b0b6b32d715d7d15bc8
LanternTi <2121889336@qq.com>
2022-11-28 14:34:25 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <RouterView></RouterView>
+</template>
+
+<style scoped>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    padding: unset;
+    margin-left: unset;
+    margin-right: unset;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import './style.css'
+import { router } from 'route/routes'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import App from './App.vue'
+
+const pinia = createPinia()
+const app = createApp(App)
+pinia.use(piniaPluginPersistedstate)
+app.use(router).use(pinia).mount('#app')
